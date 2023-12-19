13230c93b4f6445f8b184dbd1374bd0456537933
Aexey Kostenkov <l99shka@icloud.com>
2023-12-19 18:07:30 +0300

    Изменения в области корзины 2.2

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Cart;
 use App\Models\Category;
 use App\Models\Product;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class MainController extends Controller
